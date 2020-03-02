efd91ee8ff9833b4a42b1c93b5d942797ca146ac
Alex Hooper <alexanderghooper@gmail.com>
2020-03-02 22:03:03 +0000

    sc for app

---
+++ src/styled.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.main`
+  display: flex;
+  opacity: ${props => props.mounted ? '1' : '0'};
+  transform: translate3d(0, ${props => props.mounted ? '0px' : '-50px'}, 0);
+  transition: all 0.4s;
+  flex-flow: column;
+  padding: 45px 40px;
+  border-top: 3px solid #289bb1;
+  width: 90%;
+  margin: 50px auto 0;
+  background: #2d396552;
+  border-radius: 5px;
+  box-shadow: 0 2.8px 2.8px rgba(0, 0, 0, 0.022), 0 6.7px 6.7px rgba(0, 0, 0, 0.032), 0 12.5px 12.6px rgba(0, 0, 0, 0.04), 0 22.3px 22.6px rgba(0, 0, 0, 0.048), 0 41.8px 42.2px rgba(0, 0, 0, 0.058), 0 100px 101px rgba(0, 0, 0, 0.08);
+  box-sizing: border-box;
+`;
+
+export const TwoColRow = styled.section`
+  display: flex;
+  padding: 30px 0;
+  border-bottom: 1px solid #475179;
+`;
