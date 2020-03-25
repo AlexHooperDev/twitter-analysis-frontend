b48e142a6cff811ffb87d781361ff38168a735da
Alex Hooper <alexanderghooper@gmail.com>
2020-03-25 17:22:19 +0000

    linting changes

--- src/containers/MostNegativeTweet/index.js
+++ src/containers/MostNegativeTweet/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import { Wrapper, Tweet } from './styled';
-import { Title } from '../../components/Title';
-import { Negative } from '../../components/Negative';
+import { Wrapper, Tweet } from "./styled";
+import { Title } from "../../components/Title";
+import { Negative } from "../../components/Negative";
 
 export const MostNegativeTweet = ({ tweet }) => {
   return (
-    <Wrapper><Title copy="Most Negative Tweet" /><Negative percentage={tweet.sentimentScore.Negative.toFixed(3)*100} /><Tweet>{tweet.tweet}</Tweet></Wrapper>
+    <Wrapper>
+      <Title copy="Most Negative Tweet" />
+      <Negative percentage={tweet.sentimentScore.Negative.toFixed(2) * 100} />
+      <Tweet>{tweet.tweet}</Tweet>
+    </Wrapper>
   );
 };
